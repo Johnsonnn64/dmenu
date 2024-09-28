db81e1bf69eb94b687914281591e6da216db7bb3
JohnChad <naktacamel2@gmail.com>
2024-09-28 16:10:25 -0700

    font change

--- config.h
+++ config.h
@@ -5,6 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono Nerd Font:style=Medium:size=10:antialias=true:autohint=true"
+  // "Iosevka Nerd Font:style=Medium:size=11:antialias=true",
+   "Symbols Nerd Font Mono:style=2048-em:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
